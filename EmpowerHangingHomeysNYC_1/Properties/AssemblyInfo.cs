5c03eeb6db884c42cd4773318e15672011f0e444
joonipar <51436299+joonipar@users.noreply.github.com>
2023-05-16 11:30:45 -0700

    Add project files.

---
+++ EmpowerHangingHomeysNYC_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerHangingHomeysNYC_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerHangingHomeysNYC_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("B1CB85A6-8794-433E-94E5-4070CD930658")]
+[assembly: AssemblyVersion("1.0.0.0")]
